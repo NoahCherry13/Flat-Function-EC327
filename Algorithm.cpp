7d741d83b44dcb8533524879ccd240e05dd54c46
SuperMusey <rishavde.1729@gmail.com>
2021-11-22 16:54:37 -0500

    Initial commit for algorithm code in c++
    First commit

---
+++ Algorithm.cpp
@@ -0,0 +1,9 @@
+//Use a greedy algorithm to traverse the csv files from the web scraper
+// 1) CSV file read from the output of web scraper
+// 2) Store the info in the member variable into the object variables
+// 3) Traverse using probably a binary tree algorith or doubly linked list 
+
+int main()
+{
+	return 0;
+}
