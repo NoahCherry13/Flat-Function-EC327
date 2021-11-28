70a23e8ec1d60c494c8ad1acbbb75a32efbbd431
ayak2002 <ayak@bu.edu>
2021-11-27 22:29:52 -0500

    Updated testFindConflicts.cpp (testing)
    Main function has been updated (for testing purposes).

--- testFindConflicts.cpp
+++ testFindConflicts.cpp
@@ -7,9 +7,9 @@ int main()
     vector <string> course_list = {"ENGEC327", "ENGEC311", "ENGEC330", "CASMA226","CASPY212"};
     vector <string> names = {"Densmore", "Joshi", "Moreshet", "Lin", "Butler"};
     vector <double> ratings = {4.5, 4.3, 3.8, 4.0, 4.3};
-    vector <int> start_times = {230, 1220, 900, 1010, 930};
-    vector <int> end_times = {415, 205, 1045, 1100, 1100};
-    vector <vector<char>> days = {{'M', 'W'}, {'T', 'R'}, {'T', 'R'}, {'M', 'W', 'F'}, {'T', 'R'}};
+    vector <int> start_times = {1430, 1220, 900, 1010, 930};
+    vector <int> end_times = {1615, 1405, 1045, 1100, 1045};
+    vector <vector<char>> days = {{'M', 'W'}, {'F'}, {'T', 'R'}, {'M', 'W', 'F'}, {'T', 'R'}};
 
     Professor Prof1(course_list.at(0), names.at(0), ratings.at(0), start_times.at(0), end_times.at(0), days.at(0));
     Professor Prof2(course_list.at(1), names.at(1), ratings.at(1), start_times.at(1), end_times.at(1), days.at(1));
@@ -24,16 +24,21 @@ int main()
     Professor* ptr5 = &Prof5;
 
     vector <Professor*> prof_ptrs = {ptr1, ptr2, ptr3, ptr4, ptr5};
+
+
+
     vector <vector<Professor*>> sameDay_ptrs = getSameDay(prof_ptrs);
 
-    for (vector <vector<Professor*>> :: iterator it = sameDay_ptrs.begin(); it != sameDay_ptrs.end(); it++)
+    //testing hasTimeConflict function
+    if(hasTimeConflicts(prof_ptrs))
     {
-        cout << "professors on same day: " << endl;
-        for (vector <Professor*> :: iterator it2 = (*it).begin(); it2!=(*it).end(); it2++)
-        {
-            cout << (*it2)->getName() << endl;
-        }
+        cout << "Schedule contains time conflicts" << endl;
     }
+    else
+    {
+        cout << "Schedule contains no time conflicts" << endl;
+    }
+
     return 0;
 
 }
